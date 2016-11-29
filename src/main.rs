a848567139b035adff0689645db5f514426fd502
green4free <ninjalostinsnow@gmail.com>
2016-11-29 14:13:33 +0100

    Added loop

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-	println!("Guess the number!");
 	let secret_number = rand::thread_rng().gen_range(0, 100) + 1;
 	println!("Number {}", secret_number);
-	println!("Input: ");
-	let mut guess = String::new();
-	io::stdin().read_line(&mut guess).expect("Failed to read line");
-	let guess: u32 = guess.trim().parse().expect("Please type a number");
-	
-	println!("you guessed: {}", guess);
-	match guess.cmp(&secret_number) {
-	Ordering::Less => println!("Too small!"),
-	Ordering::Greater => println!("Too big!"),
-	Ordering::Equal => println!("You win!"),
+	println!("Guess the number!");
+	loop {
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess).expect("Failed to read line");
+		let guess: u32 = guess.trim().parse().expect("Please type a number");
+		
+		println!("you guessed: {}", guess);
+		match guess.cmp(&secret_number) {
+		Ordering::Less => println!("Too small!"),
+		Ordering::Greater => println!("Too big!"),
+		Ordering::Equal => {
+			println!("You win!");
+			break;
+			}
+		}
+		println!("Guess again!");
 	}
 }
