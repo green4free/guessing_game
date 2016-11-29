98f0efa04a180d31aab9acede155e11178d115ce
green4free <ninjalostinsnow@gmail.com>
2016-11-29 13:51:01 +0100

    Added number comparison and string to number parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 
+use std::cmp::Ordering;
 use rand::Rng;
 use std::io;
 
@@ -10,5 +11,12 @@ fn main() {
 	println!("Input: ");
 	let mut guess = String::new();
 	io::stdin().read_line(&mut guess).expect("Failed to read line");
+	let guess: u32 = guess.trim().parse().expect("Please type a number");
+	
 	println!("you guessed: {}", guess);
+	match guess.cmp(&secret_number) {
+	Ordering::Less => println!("Too small!"),
+	Ordering::Greater => println!("Too big!"),
+	Ordering::Equal => println!("You win!"),
+	}
 }
